18f61a3c202b069de7d67ebea127d7e4676f3190
gdbecker <53883172+gdbecker@users.noreply.github.com>
2024-01-04 11:14:31 -0500

    Update ProjectCard.js

--- showcase/app/components/ProjectCard.js
+++ showcase/app/components/ProjectCard.js
@@ -62,7 +62,7 @@ function ProjectCard({ index, id, img, title, code_url, type }) {
 
   return (
     <div className={`flex w-full flex-col rounded-md overflow-hidden bg-white ${ getOutlineClass(type) } shadow-[0_0px_4px]`} x-intersect="$el.classList.add('flip-in-ver-left')">
-      <div className={`flex flex-row h-44 project-background url(../../public/${ img }.jpg)`}></div>
+      <div className={`flex flex-row h-44 project-background bg-[url('../public/images/${ img }.jpg')]`}></div>
       <div className="flex-auto grid grid-cols-1 px-5 py-3 pb-1 text-gray">
         <div className="flex-row">
           <Link 
