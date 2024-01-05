606c463347e493831a7bac4fcead6afeac0130b6
gdbecker <53883172+gdbecker@users.noreply.github.com>
2024-01-05 11:44:31 -0500

    Update Footer.js

--- showcase/app/components/Footer.js
+++ showcase/app/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
   return (
     <div className="h-fit p-5 font-interRegular text-center text-white text-sm border-lightGray border-t-2 bg-gray 2xl:px-36">
-      Designed and developed with ❤️ by <a href="https://github.com/gdbecker/AnalyticsPortfolio/tree/main" target="_blank" className="font-interRegular text-mediumYellow hover:text-lightYellow">Garrett Becker</a>. &copy; ${new Date().getFullYear()} All rights reserved.
+      Designed and developed with ❤️ by <a href="https://github.com/gdbecker/AnalyticsPortfolio/tree/main" target="_blank" className="font-interRegular text-mediumYellow hover:text-lightYellow">Garrett Becker</a>. &copy; {new Date().getFullYear()} All rights reserved.
     </div>
   )
 }
