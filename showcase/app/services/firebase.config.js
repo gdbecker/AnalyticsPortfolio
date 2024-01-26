92446f3825d086f72037711b4506d5d937b07a86
Garrett Becker <garrettdbecker@gmail.com>
2024-01-26 14:32:18 -0500

    Showcase Updates

---
+++ showcase/app/services/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: "analyticsportfolio-fdf8e",
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
