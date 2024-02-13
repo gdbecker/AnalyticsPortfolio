1ef96afd010e6600d17b661dca5c11b76ff29e93
Garrett Becker <garrettdbecker@gmail.com>
2024-02-13 15:44:59 -0500

    Showcase Updates

--- showcase/app/components/Navigation.js
+++ showcase/app/components/Navigation.js
@@ -9,14 +9,14 @@ function Navigation() {
     <div class="flex h-16 p-10 items-center justify-between overflow-hidden border-lightGray border-b-2 bg-gray 2xl:px-36">
       <div className="flex items-center justify-between">
         <a class="flex items-center justify-center" href="/">
-          <h1 className="font-interBold tracking-wide text-sm text-white md:text-lg">Analytics Projects</h1>
+          <h1 className="font-interBold tracking-wide text-xs text-white md:text-lg">Analytics Projects</h1>
           <Image 
             src={Logo}
             alt="Frontend Mentor Logo"
             className="pr-2 mx-2 w-10 h-8 border-lightGray border-r-2 md:w-12 md:h-10 md:mx-none"
           />
         </a>
-        <h1 className="font-interRegular tracking-wider text-sm text-lightYellow md:text-lg">Showcase</h1>
+        <h1 className="font-interRegular tracking-wider text-xs text-lightYellow md:text-lg">Showcase</h1>
       </div>
       <a class="flex items-center justify-center" href="https://github.com/gdbecker/AnalyticsPortfolio/tree/main" target="_blank">
         <Image 
--- showcase/app/components/ProjectCard.js
+++ showcase/app/components/ProjectCard.js
@@ -12,14 +12,14 @@ function ProjectCard({ id, img, title, code_url, types }) {
 
   function getLabel(type) {
     return (
-      <p className={`px-2 h-full w-fit my-1 ${getCSS('label', type)} ring-2 rounded-sm font-interBold text-sm overflow-hidden lg:text-[0.75rem]`}>{type.toUpperCase()}</p>
+      <p className={`px-2 h-full my-1 ${getCSS('label', type)} ring-2 rounded-sm font-interBold text-sm text-right overflow-hidden lg:text-[0.75rem]`}>{type.toUpperCase()}</p>
     );
   }
 
   return (
     <div className={`flex w-full flex-col rounded-md overflow-hidden bg-white ${getCSS('outline', types[0])} shadow-[0_0px_4px]`} x-intersect="$el.classList.add('flip-in-ver-left')">
       <div 
-        className={"flex flex-row h-44 bg-top bg-no-repeat bg-cover"}
+        className={"flex flex-row h-44 bg-top bg-no-repeat bg-cover bg-project"}
         style={{
           backgroundImage: `url(${ img }.jpg)`
         }}
@@ -33,10 +33,10 @@ function ProjectCard({ id, img, title, code_url, types }) {
             }}
             className="flex py-2 text-xl font-interBold relative no-underline lg:text-md"><span className={`link-shadow-${types[0].replace(" ","")}`}>{title}</span></Link>
         </div>
-        <div className="grid grid-cols-2 items-end justify-between w-full py-3 font-interRegular text-md lg:text-sm">
+        <div className="grid grid-cols-2 items-end justify-between py-3 font-interRegular text-md lg:text-sm">
           <Link href={code_url} target="_blank" className={`flex flex-row project-link text-lg ${getCSS('hover', types[0])}`}><FaGithub /> <p className="text-sm pl-2">Learn More</p></Link>
           <div className="flex flex-col items-end justify-end">
-            <div className="flex flex-col items-end">
+            <div className="flex flex-col items-end w-8/12 md:w-fit">
               {types.map((t) => getLabel(t))}
             </div>
           </div>
